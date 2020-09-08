73922e29a2b97db7fdbd65e8db126304f74e789c
Adamlancaster.al@gmail.com <Adamlancaster.al@gmail.com>
2020-09-08 11:59:44 -0400

    added functions and corrected comments.

--- 3-8_importPyp.py
+++ 3-8_importPyp.py
@@ -1,26 +1,25 @@
 # Adam Lancaster
 # this program displays and manipulates the contents of the clipboard.
-# for best results, copy something to the clipboard before running program,
-# otherwise output will be inconsistent.
 
 import pyperclip as pc
 
 def main():
+# main function loads the clipboard with a message, then prints the contents
+# of the clipboard. it then repeats this process with the second message. 
 
-    pc.copy('this is the first message.')
+    clipboardPnP('this is the first message.')
+    clipboardPnP('this is the second message.')
+
+def clipboardPnP(statement):
+# clipboard paste and printer. I couldnt think of something better to call
+# it, and clipboardPasteAndPrint is way too long. function loads statement 
+# into clipboard then prints the contents of the clipboard.
+
+    pc.copy(statement)
     # this function copies the msg txt to clipboard.
-
     clipboard = pc.paste()
     # this function copies clipboard txt to variable.
-
     print(clipboard)
     # prints contents of clipboard.
 
-    pc.copy('this is the second message.')
-
-    clipboard = pc.paste()
-
-    print(clipboard)
-
 main()
-
